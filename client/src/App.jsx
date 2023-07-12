f9cbfb8c81737624ad44d62e86d0720934a8b740
junaiyetmia470@gmail.com <rezowan.7890@gmail.com>
2023-07-12 08:54:05 -0500

    todo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,88 +1,102 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-const [text,setText] = useState('')
-const [editText,setEditText] = useState(false)
-const [edit,setEdit] = useState({})
-const [id,setId] = useState('')
-const [textCategory,setTextCategory] = useState('')
-const [list,setList] = useState([])
-const [categorylist,setCategoryList] = useState([])
-const [active,setActive]= useState(false)
+  const [text, setText] = useState("");
+  const [editText, setEditText] = useState(false);
+  const [edit, setEdit] = useState({});
+  const [editTask, setEditTask] = useState("");
+  const [id, setId] = useState("");
+  const [textCategory, setTextCategory] = useState("");
+  const [list, setList] = useState([]);
+  const [categorylist, setCategoryList] = useState([]);
+  const [active, setActive] = useState(false);
   //http://localhost:8000/addtodo
   //http://localhost:8000/alltodo
   //http://localhost:8000/createcategory
   //http://localhost:8000/allcategory
   //http://localhost:8000/edittodo
+  //http://localhost:8000/delete
 
-  useEffect(()=>{
+  useEffect(() => {
     async function all() {
       let data = await axios.get("http://localhost:8000/alltodo");
       let categorydata = await axios.get("http://localhost:8000/allcategory");
-      setList(data.data)
-      setCategoryList(categorydata.data)
+      setList(data.data);
+      setCategoryList(categorydata.data);
     }
-    all()
-  },[active])
-  let handleSubmit= async()=>{
-        let data = await axios.post("http://localhost:8000/addtodo",{
-          name:text,
-          category : id,
-        })
-        setActive(!active)
-       console.log(data)
-  }
-  let handleCreateCategory = async()=>{
-    let data = await axios.post("http://localhost:8000/createcategory",{
-      name:textCategory,
-      
-    })
-    setActive(!active)
-    console.log(data)
-  }
-  let handleId = (e)=>{
-    setId(e.target.value)
-  }
-  let handleEdit = (item)=>{
-    setEditText(true)
-    setEdit(item)
-  }
+    all();
+  }, [active]);
+  let handleSubmit = async () => {
+    let data = await axios.post("http://localhost:8000/addtodo", {
+      name: text,
+      category: id,
+    });
+    setActive(!active);
+    console.log(data);
+  };
+  let handleCreateCategory = async () => {
+    let data = await axios.post("http://localhost:8000/createcategory", {
+      name: textCategory,
+    });
+    setActive(!active);
+    console.log(data);
+  };
+  let handleId = (e) => {
+    setId(e.target.value);
+  };
+  let handleEdit = (item) => {
+    setEditText(true);
+    setEdit(item);
+  };
+  let handleUpdate = async () => {
+    console.log(edit._id);
+    let data = await axios.post("http://localhost:8000/edittodo", {
+      id: edit._id,
+      name: edit.name,
+    });
+    setActive(!active);
+    console.log(data);
+  };
+
+  let handleDelete = async (item) => {
+    let data = await axios.post("http://localhost:8000/delete", {
+      id: item._id,
+    });
+    setActive(!active);
+    console.log(data);
+  };
   return (
     <>
-     <h1>ToDo App</h1>
-     <>
-     <input onChange={(e)=>setText(e.target.value)} value={editText&& edit.name}/>
-
-     {editText
-     ?
-     <button>Update</button>
-      :
-      <button onClick={handleSubmit}>Submit</button>
-    }
-     <select onChange={handleId}>
-      <option >Select Category</option>
-      {categorylist.map(item=>(
-        <option key={item.name} value={item._id}>{item.name}</option>
-      ))}
-     </select>
-     <br />
-     <br /> 
-     <input onChange={(e)=>setTextCategory(e.target.value)}/>
-     <button onClick={handleCreateCategory}>Creat Catagory</button>
-    <h1>Tod List</h1>
-     <ul>  
-      {list.map(item=>(
-        <li key={item.name}>
-          {item.name}--{item.category.name} 
-           <button onClick={()=>handleEdit(item)}>Edit</button>
-        </li>
-
-      ))}
-     </ul>
-     </>
+      <h1>ToDo App</h1>
+      <>
+        <input onChange={(e) => (editText ? setEdit({ ...edit, name: e.target.value }) : setText(e.target.value))} value={editText ? edit.name : text} />
+        {editText ? <button onClick={handleUpdate}>Update</button> : <button onClick={handleSubmit}>Submit</button>}
+        <select onChange={handleId}>
+          <option>Select Category</option>
+          {categorylist.map((item) => (
+            <option key={item.name} value={item._id}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <input onChange={(e) => setTextCategory(e.target.value)} />
+        <button onClick={handleCreateCategory}>Creat Catagory</button>
+        <h1>Tod List</h1>
+        <ul>
+          {list.map((item) => (
+            <li key={item.name}>
+              {item.name}--{item.category.name}
+              <button onClick={() => handleEdit(item)}>Edit</button>
+              <button onClick={() => handleDelete(item)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,63 +1,63 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const mongoose = require('mongoose');
-const TodoTask = require("./models/todoSchema.js")
-const Category = require("./models/categorySchema.js")
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const TodoTask = require("./models/todoSchema.js");
+const Category = require("./models/categorySchema.js");
 
-mongoose.connect(`mongodb+srv://todo:U7xFRp9NbglgOpHw@cluster0.wv9fbmz.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
-    console.log('DB Connected')
-})
-app.use(cors())
-app.use(express.json())
+mongoose.connect(`mongodb+srv://todo:U7xFRp9NbglgOpHw@cluster0.wv9fbmz.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+  console.log("DB Connected");
+});
+app.use(cors());
+app.use(express.json());
 
-app.post("/addtodo",(req,res)=>{
-    const {name,category}= req.body
-    let todo = new TodoTask({
-        name,
-        category
-    })
-    todo.save()
-    res.send(todo)
-})
+app.post("/addtodo", (req, res) => {
+  const { name, category } = req.body;
+  let todo = new TodoTask({
+    name,
+    category,
+  });
+  todo.save();
+  res.send(todo);
+});
 
-app.post("/createcategory",(req,res)=>{
-    const {name}= req.body
-    let category = new Category({
-        name
-    })
-    category.save()
-    res.send(category)
-})
+app.post("/createcategory", (req, res) => {
+  const { name } = req.body;
+  let category = new Category({
+    name,
+  });
+  category.save();
+  res.send(category);
+});
 
-app.post("/edittodo",async(req,res)=>{
-    const {id,name} = req.body
+app.post("/edittodo", async (req, res) => {
+  const { id, name } = req.body;
 
-    let updateTodo = await TodoTask.findOneAndUpdate({_id:id},{name},{new:true})
+  let updateTodo = await TodoTask.findOneAndUpdate({ _id: id }, { name }, { new: true });
 
-    res.send(updateTodo)
-})
-
-
+  res.send(updateTodo);
+});
 
 /**/
-app.get("/alltodo", async(req,res)=>{
-    let data = await TodoTask.find({}).populate("category");
-    res.send(data)
-})
+app.get("/alltodo", async (req, res) => {
+  let data = await TodoTask.find({}).populate("category");
+  res.send(data);
+});
 
-app.get("/allcategory",async(req,res)=>{
-    let data = await Category.find({})
-    res.send(data)
-})
+app.get("/allcategory", async (req, res) => {
+  let data = await Category.find({});
+  res.send(data);
+});
 
+app.post("/delete", async (req, res) => {
+  const { id } = req.body;
+  await TodoTask.findByIdAndDelete(id);
 
-
-
-
+  res.send("Delete");
+});
 
 app.listen("8000", () => {
-    console.log("Port Start")
-  })
+  console.log("Port Start");
+});
 
-  //U7xFRp9NbglgOpHw
+//U7xFRp9NbglgOpHw
